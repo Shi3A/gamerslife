d7e6a39603811a58707fac1b92a5faf5cee46779
Soshnikov Artem <213036@skobka.com>
2015-08-05 02:42:13 +0600

    implement rating app into Post model

--- post/models.py
+++ post/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 from base.models import Page
 from user.models import User
+from django.apps import apps
 
 
 class Post(Page):
     author = models.ForeignKey(User, verbose_name="Author")
     original_file = models.CharField(verbose_name="Uploaded file", blank=False, null=False, max_length=512)
     converted_file = models.CharField(verbose_name="Converted file", blank=True, null=True, max_length=512)
+
+    _rating_conf = apps.app_configs.get('rating')
+    _rating_driver = _rating_conf.driver
+    """:type : rating.base.Base """
 
     def get_file(self):
         if self.converted_file:
@@ -19,6 +24,12 @@ class Post(Page):
             # TODO: send signal to convert
             pass
         super(Post, self).save(*args, **kwargs)
+
+    def ratingIncr(self):
+        self._rating_driver.incr()
+
+    def ratingDecr(self):
+        self._rating_driver.decr()
 
     class Meta:
         verbose_name = "Post"
